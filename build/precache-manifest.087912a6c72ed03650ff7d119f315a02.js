396e67d180590e7f63da923b704f2d194e7ade64
Spyder2603 <123saurabh2603@gmail.com>
2020-07-05 18:28:56 +0530

    build

---
+++ build/precache-manifest.087912a6c72ed03650ff7d119f315a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef550cc5f8490852b99fae45e21d930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae41f317ba894b0d8f65",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "a7428b991dc01e6c7346",
+    "url": "/static/js/2.2eafe54b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2eafe54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae41f317ba894b0d8f65",
+    "url": "/static/js/main.d551c62a.chunk.js"
+  },
+  {
+    "revision": "17228d34bec2d0821e3f",
+    "url": "/static/js/runtime-main.dc20b871.js"
+  }
+]);
